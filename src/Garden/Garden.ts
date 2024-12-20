40d34b73e1b4851ab65f3a4f73853b7879e73dea
Carlos Díaz <linkmetal18@gmail.com>
2024-12-20 15:56:27 +0000

    green: implemented turn on

--- src/Garden/Garden.ts
+++ src/Garden/Garden.ts
@@ -7,12 +7,22 @@ export class Garden {
       .map(() => Array(1000).fill(false))
   }
 
-  turnOn(arg0: number[], arg1: number[]): any {
-    if (arg0[0] > 1000 || arg0[1] > 1000 || arg1[0] > 1000 || arg1[1] > 1000) {
+  turnOn(initialCoords: number[], endCoords: number[]): any {
+    if (initialCoords[0] > 1000 || initialCoords[1] > 1000 || endCoords[0] > 1000 || endCoords[1] > 1000) {
       throw new Error("Invalid bulb number")
     }
 
-    this.garden[0][0] = true
+    this.garden = this.garden.map((row, x) => {
+      if (x >= initialCoords[0] && x <= endCoords[0]) {
+        return row.map((light, y) => {
+          if (y >= initialCoords[1] && y <= endCoords[1]) {
+            return true
+          }
+          return light
+        })
+      }
+      return row
+    })
   }
 
   getLightsOn(): number {
